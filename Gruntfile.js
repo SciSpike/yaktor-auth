7764c3216a3f29fca3d44f42c6fc34f4bc233c75
JK <jonathan.kamke@gmail.com>
2016-06-24 16:46:57 -0700

    upstream

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,6 +40,10 @@ module.exports = function (grunt) {
       'add-owner': {
         command: [ 'npm owner add', grunt.option('owner'), packageJson.name, registry ].join(' ')
       },
+      'create-maintenance-branch':{
+        command:['git checkout -b ' + newTag + ' ' + tag,
+          'git branch --set-upstream-to=origin/' + newTag + ' ' + newTag].join('&&')
+      },
       'release-minor': {
         'command': [
           "[ $(git status | head -n 1 | awk '{ print $3 }') == '" + master + "' ]", // minors only from master branch
@@ -47,7 +51,7 @@ module.exports = function (grunt) {
           'git diff --cached --exit-code --no-patch', // no modified files
           'grunt bump:minor',
           'grunt shell:publish',
-          'git checkout -b ' + newTag + ' ' + tag,
+          'grunt shell:create-maintenance-branch',
           'grunt bump:prepatch --no-tag',
           'git checkout master',
           'grunt bump:preminor --no-tag'
